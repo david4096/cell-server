69baa04af0207f272715b88e48d9ff1572abb815
David Steinberg <david@resium.com>
2017-07-26 15:27:51 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     namespace_packages=["celldb"],
     zip_safe=False,
     url="https://github.com/david4096/celldb",
-    version=1.0,
+    version=2.0,
     entry_points={
         'console_scripts': [] # TODO add one for SQL line
     },
